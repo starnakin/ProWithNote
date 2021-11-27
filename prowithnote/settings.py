b81374d54df17f02e74ae281db60dd08bf76a691
starnakin <47717174+starnakin@users.noreply.github.com>
2021-11-27 15:04:06 -0500

    Update settings.py

--- prowithnote/settings.py
+++ prowithnote/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b*dznx9&#tzzh$+q*wj9-se(r$xs#e6cqs^wgzc64880(gz=%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.142"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
